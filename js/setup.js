57f444064798b559acc508cfb4366f91a606a5c5
Tom <mckeont@upenn.edu>
2022-09-05 12:18:26 -0400

    Added data files

---
+++ js/setup.js
@@ -0,0 +1,14 @@
+var map = L.map('map', {
+  center: [39.36002827371172, -75.87027121541223],
+  zoom: 18,
+});
+
+// var Satellite = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+
+var Satellite = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+}).addTo(map);
