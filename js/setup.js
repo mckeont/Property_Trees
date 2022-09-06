e8b79f1d50bedc42b00cca6b9c6ade688d35ce16
Tom <mckeont@upenn.edu>
2022-09-06 12:34:36 -0400

    update setup.js

--- js/setup.js
+++ js/setup.js
@@ -8,7 +8,12 @@ var map = L.map('map', {
 // }).addTo(map);
 
 
-var Satellite = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
-    maxZoom: 20,
-    subdomains:['mt0','mt1','mt2','mt3']
+// var Satellite = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+//     maxZoom: 20,
+//     subdomains:['mt0','mt1','mt2','mt3']
+// }).addTo(map);
+
+
+var Satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
 }).addTo(map);
